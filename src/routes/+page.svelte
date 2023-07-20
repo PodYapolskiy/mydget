b9b0ae981f5ad83d583e22172bb1ea02667912b2
pferdlexxie <alexandrakuzmich@mail.ru>
2023-07-21 00:50:56 +0300

    ✨ feat: home page and footer created

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
-<script>
-    import Footer from '$lib/components/Footer.svelte';
+<script lang="ts">
+    import CommonWords from '$lib/components/CommonWords.svelte';
+    import TechnologicalStack from '$lib/components/TechnologicalStack.svelte';
     import Team from '$lib/components/Team.svelte';
     import Welcome from '$lib/components/Welcome.svelte';
 </script>
 
 <Welcome />
+<CommonWords />
+<TechnologicalStack />
 <Team />
-<Footer />
