ae06b1fd5e73ce78b13933d58b05bd15fa2c4577
cuprum-acid <jony.zh@yandex.ru>
2023-07-21 00:16:16 +0300

    👢 feat: added Footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,29 @@
+<footer class="bg-gray-800 text-white p-6">
+    <div class="container mx-auto">
+        <div class="flex flex-col md:flex-row items-center justify-between">
+            <div class="mb-4 md:mb-0">
+                <span class="font-bold"
+                    >Mydget — keep an eye on your budget</span
+                >
+                <p class="text-gray-400">
+                    Universitetskaya St., 1, Innopolis, Tatarstan Republic,
+                    Russia, 420500
+                </p>
+                <p class="text-gray-400">Telegram: @podyapolskiyaa</p>
+                <p class="text-gray-400">Email: podyapolskiyaa@gmail.com</p>
+            </div>
+            <div>
+                <ul class="flex space-x-4">
+                    <li><a href="#!" class="hover:text-gray-300">Home</a></li>
+                    <li><a href="#!" class="hover:text-gray-300">About</a></li>
+                    <li>
+                        <a href="#!" class="hover:text-gray-300">Services</a>
+                    </li>
+                    <li>
+                        <a href="#!" class="hover:text-gray-300">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
