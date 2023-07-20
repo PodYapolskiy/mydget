72e0722cacb9a1107ccbe4eeee4bfb3a04436b95
cuprum-acid <jony.zh@yandex.ru>
2023-07-20 23:57:50 +0300

    🩹 fix: returned import line from layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import '../app.css';
+    import App from './fb';
     import { onMount } from 'svelte';
     import { getAuth, onAuthStateChanged } from 'firebase/auth';
     import { goto } from '$app/navigation';
