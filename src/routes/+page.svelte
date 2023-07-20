6c0d0806b6abf2eb6a591275dd1271201d99df5f
cuprum-acid <jony.zh@yandex.ru>
2023-07-21 00:17:43 +0300

    ✨ feat: add footer in homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
+    import Footer from '$lib/components/Footer.svelte';
     import Team from '$lib/components/Team.svelte';
     import Welcome from '$lib/components/Welcome.svelte';
 </script>
 
 <Welcome />
 <Team />
+<Footer />
