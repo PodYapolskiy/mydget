caeb3f5fe0d28c1d30eebbfd6954cdd28e9268bb
dprostiruk <67105855+dprostiruk@users.noreply.github.com>
2023-07-22 22:22:12 +0300

    :sparkles: feat: transaction display in the table

--- src/lib/components/Transaction.svelte
+++ src/lib/components/Transaction.svelte
@@ -5,9 +5,31 @@
     export let date: Date;
 </script>
 
-<div>
-    <p>{id}</p>
-    <p>{amount}</p>
-    <p>{category}</p>
-    <p>{date}</p>
-</div>
+<tr class="hover">
+    <th>
+        <label>
+            <input type="checkbox" class="checkbox" />
+        </label>
+    </th>
+    <td>
+        <div class="flex items-center space-x-3">
+            <div class="avatar">
+                <div class="mask mask-squircle w-12 h-12">
+                    <img src="/food.png" alt="food" />
+                </div>
+            </div>
+            <div>
+                <div class="font-bold">{category}</div>
+            </div>
+        </div>
+    </td>
+    <td>
+        {amount}
+        <br />
+        <span class="badge badge-ghost badge-sm">{id}</span>
+    </td>
+    <td>{date}</td>
+    <th>
+        <button class="btn btn-ghost btn-xs">details</button>
+    </th>
+</tr>
