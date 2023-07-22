9e638cc7645c7107f46d1262c3e1399495bb82fd
Ivan <ivangolov4@gmail.com>
2023-07-22 11:10:01 +0300

    🤡 future fix: need fix bug in Login form

--- src/lib/components/LoginForm.svelte
+++ src/lib/components/LoginForm.svelte
@@ -8,6 +8,7 @@
         GoogleAuthProvider,
         signInWithPopup
     } from 'firebase/auth';
+    
     import { addDoc, collection } from 'firebase/firestore';
     import { db } from '../../routes/fb';
 
