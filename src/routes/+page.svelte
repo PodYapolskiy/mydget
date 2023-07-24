07aa7b13343f506c48b44fce441b9ec53be00e48
Ivan <ivangolov4@gmail.com>
2023-07-24 11:41:29 +0300

    🩹 fix: fix little bug in home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -401,7 +401,7 @@
                         type="email"
                         autocomplete="email"
                         required
-                        class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                        class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-black shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
                         placeholder="Enter your email"
                     />
                     <button
