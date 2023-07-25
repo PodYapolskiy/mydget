9a1a4d9cc371182273cfb4574f935ecb3cad22c0
PodYapolsky <podyapolskiyaa@gmail.com>
2023-07-25 04:55:22 +0300

    🔍 feat: try to implement ssr

--- src/routes/transactions/[id]/+page.ts
+++ src/routes/transactions/[id]/+page.ts
@@ -1,14 +1,9 @@
-import { dev } from '$app/environment';
 import { Timestamp } from 'firebase/firestore';
 
 import type { PageLoad } from './$types';
 
-// we don't need any JS on this page, though we'll load
-// it in dev so that we get hot module replacement
-export const csr = dev;
-
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
+export const csr = true;
+export const ssr = true;
 export const prerender = false;
 
 export const load = (({ params }) => {
